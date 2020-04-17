885d9a64c0fd29bcddf2337dc01704fbfc4fc472
Denis Medeiros <48757658+DenisMedeirosKollosso@users.noreply.github.com>
2020-04-17 11:09:54 -0300

    use template

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -11,7 +11,10 @@ interface Balance {
 @EntityRepository(Transaction)
 class TransactionsRepository extends Repository<Transaction> {
   public async getBalance(): Promise<Balance> {
-    // TODO
+    const findTransaction = await this.findOne({
+      where: { id },
+    });
+    return findTransaction;
   }
 }
 
